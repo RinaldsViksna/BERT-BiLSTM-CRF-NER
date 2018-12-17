a8ffd25b2303a3d4a7a85a739f146072017645a5
dsindex <hazzling@gmail.com>
2018-12-17 19:13:37 +0900

    -

--- merge.py
+++ merge.py
@@ -32,19 +32,12 @@ if __name__ == '__main__':
             bucket = []
         else: bucket.append(line)
 
-    '''
-    print(len(a_data))
-    print(len(b_data))
-    for idx in range(len(a_data)):
-        print(len(a_data[idx]), len(b_data[idx]))
-    sys.exit(0)
-    '''
+    assert(len(a_data) == len(b_data))
 
-    idx = 0
+    m_data = []
     for idx, _ in enumerate(a_data):
         if len(a_data[idx]) != len(b_data[idx]):
             sys.stderr.write('merge error!')
-            break
             print('[before]')
             for a_line in a_data[idx-1]:
                 print(a_line)
@@ -66,13 +59,44 @@ if __name__ == '__main__':
             for b_line in b_data[idx+1]:
                 print(b_line)
             print('==========================================')
+            sys.exit(1)
 
+        entry = []
+        a_word_list = []
         for a_line, b_line in zip(a_data[idx], b_data[idx]):
             a_tokens = a_line.split()
-            if a_tokens[3] == 'X': continue
-            if b_line == 'X': b_line = 'O'
-            print(a_line, b_line)
+            assert(len(a_tokens) == 4)
+            a_word = a_tokens[0].replace('##','')
+            a_pos  = a_tokens[1]
+            a_chunk = a_tokens[2]
+            a_label = a_tokens[3]
+            b_label = b_line
+            if b_label == 'X': b_label = 'O'
+            tp = (a_word, a_pos, a_chunk, a_label, b_label)
+            entry.append(tp)
+        m_data.append(entry)
+        
+    for idx, entry in enumerate(m_data):
+        size = len(entry)
+        seq = 0
+        while seq != size:
+            tp = entry[seq]
+            word, pos, chunk, label, predict = tp
+            rng = 1
+            if seq + rng >= size:
+                print(word, pos, chunk, label, predict)
+                break
+            n_seq = seq + rng
+            n_tp = entry[n_seq]
+            n_word, n_pos, n_chunk, n_label, n_predict = n_tp
+            org_word = word
+            while n_label == 'X':
+                org_word += n_word
+                rng += 1
+                n_seq = seq + rng
+                if seq + rng >= size: break
+                n_tp = entry[n_seq]
+                n_word, n_pos, n_chunk, n_label, n_predict = n_tp
+            print(org_word, pos, chunk, label, predict)
+            seq += rng
         print('')
-        
-        
-     
