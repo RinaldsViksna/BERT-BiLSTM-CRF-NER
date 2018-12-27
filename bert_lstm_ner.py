a39c2b8afbdb05057edf456314092de9e41d319b
dsindex <hazzling@gmail.com>
2018-12-27 10:44:05 +0900

    -

--- bert_lstm_ner.py
+++ bert_lstm_ner.py
@@ -168,23 +168,44 @@ class InputFeatures(object):
         self.input_mask = input_mask
         self.segment_ids = segment_ids
         self.label_ids = label_ids
-        # self.label_mask = label_mask
 
 
-class DataProcessor(object):
-    """Base class for data converters for sequence classification data sets."""
+class NerProcessor(object):
+    """A class for data converters for sequence classification data sets."""
 
     def get_train_examples(self, data_dir):
         """Gets a collection of `InputExample`s for the train set."""
-        raise NotImplementedError()
+        return self._create_example(
+            self._read_data(os.path.join(data_dir, "train.txt")), "train"
+        )
 
     def get_dev_examples(self, data_dir):
         """Gets a collection of `InputExample`s for the dev set."""
-        raise NotImplementedError()
+        return self._create_example(
+            self._read_data(os.path.join(data_dir, "dev.txt")), "dev"
+        )
+
+    def get_test_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the test set."""
+        return self._create_example(
+            self._read_data(os.path.join(data_dir, "test.txt")), "test")
 
     def get_labels(self):
         """Gets the list of labels for this data set."""
-        raise NotImplementedError()
+        '''
+        return ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC", "X", "[CLS]", "[SEP]"]
+        '''
+        return ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC", "X"]
+
+    def _create_example(self, lines, set_type):
+        examples = []
+        for (i, line) in enumerate(lines): # line = (w, p, l)
+            guid = "%s-%s" % (set_type, i)
+            text = tokenization.convert_to_unicode(line[0])
+            pos  = tokenization.convert_to_unicode(line[1])
+            label = tokenization.convert_to_unicode(line[2])
+            examples.append(InputExample(guid=guid, text=text, pos=pos, label=label))
+        return examples
 
     @classmethod
     def _read_data(cls, input_file):
@@ -221,54 +242,7 @@ class DataProcessor(object):
             return lines
 
 
-class NerProcessor(DataProcessor):
-    def get_train_examples(self, data_dir):
-        return self._create_example(
-            self._read_data(os.path.join(data_dir, "train.txt")), "train"
-        )
-
-    def get_dev_examples(self, data_dir):
-        return self._create_example(
-            self._read_data(os.path.join(data_dir, "dev.txt")), "dev"
-        )
-
-    def get_test_examples(self, data_dir):
-        return self._create_example(
-            self._read_data(os.path.join(data_dir, "test.txt")), "test")
-
-    def get_labels(self):
-        '''
-        return ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC", "X", "[CLS]", "[SEP]"]
-        '''
-        return ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC", "X"]
-
-    def _create_example(self, lines, set_type):
-        examples = []
-        for (i, line) in enumerate(lines): # line = (w, p, l)
-            guid = "%s-%s" % (set_type, i)
-            text = tokenization.convert_to_unicode(line[0])
-            pos  = tokenization.convert_to_unicode(line[1])
-            label = tokenization.convert_to_unicode(line[2])
-            examples.append(InputExample(guid=guid, text=text, pos=pos, label=label))
-        return examples
-
-
-def write_tokens(tokens, mode):
-    if mode == "test":
-        path = os.path.join(FLAGS.output_dir, "token_" + mode + ".txt")
-        wf = codecs.open(path, 'a', encoding='utf-8')
-        for token in tokens:
-            if token != "**NULL**":
-                wf.write(token + '\n')
-        wf.close()
-
-
-def convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, mode):
-    label_map = {}
-    for (i, label) in enumerate(label_list, 1): # 0 index for '0' padding
-        label_map[label] = i
-    with codecs.open('./output/label2id.pkl', 'wb') as w:
-        pickle.dump(label_map, w)
+def convert_single_example_to_feature(ex_index, example, label_map, max_seq_length, tokenizer, mode):
     textlist  = example.text.split(' ')
     poslist   = example.pos.split(' ') 
     labellist = example.label.split(' ')
@@ -317,7 +291,6 @@ def convert_single_example(ex_index, example, label_list, max_seq_length, tokeni
     label_ids.append(0)
     input_ids = tokenizer.convert_tokens_to_ids(ntokens)
     input_mask = [1] * len(input_ids)
-    # label_mask = [1] * len(input_ids)
     # padding
     while len(input_ids) < max_seq_length:
         input_ids.append(0)
@@ -326,13 +299,10 @@ def convert_single_example(ex_index, example, label_list, max_seq_length, tokeni
         # we don't concerned about it!
         label_ids.append(0)
         ntokens.append("**NULL**")
-        # label_mask.append(0)
-    # print(len(input_ids))
     assert len(input_ids) == max_seq_length
     assert len(input_mask) == max_seq_length
     assert len(segment_ids) == max_seq_length
     assert len(label_ids) == max_seq_length
-    # assert len(label_mask) == max_seq_length
 
     if ex_index < 5:
         tf.logging.info("*** Example ***")
@@ -343,37 +313,45 @@ def convert_single_example(ex_index, example, label_list, max_seq_length, tokeni
         tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
         tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
         tf.logging.info("label_ids: %s" % " ".join([str(x) for x in label_ids]))
-        # tf.logging.info("label_mask: %s" % " ".join([str(x) for x in label_mask]))
 
     feature = InputFeatures(
         input_ids=input_ids,
         input_mask=input_mask,
         segment_ids=segment_ids,
         label_ids=label_ids,
-        # label_mask = label_mask
     )
-    write_tokens(ntokens, mode)
     return feature
 
 
+def convert_feature_to_tf_example(feature):
+    def create_int_feature(values):
+        f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+        return f
+
+    features = collections.OrderedDict()
+    features["input_ids"] = create_int_feature(feature.input_ids)
+    features["input_mask"] = create_int_feature(feature.input_mask)
+    features["segment_ids"] = create_int_feature(feature.segment_ids)
+    features["label_ids"] = create_int_feature(feature.label_ids)
+    tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+    return tf_example
+
+
 def filed_based_convert_examples_to_features(examples, label_list, max_seq_length, tokenizer, output_file, mode=None):
+    # build labe2id.pkl
+    label_map = {}
+    for (i, label) in enumerate(label_list, 1): # 0 index for '0' padding
+        label_map[label] = i
+    with codecs.open('./output/label2id.pkl', 'wb') as w:
+        pickle.dump(label_map, w)
+
+    # convert examples => features => tf_examples(tf.train.Example) => tf records(TFRecord, file)
     writer = tf.python_io.TFRecordWriter(output_file)
     for (ex_index, example) in enumerate(examples):
         if ex_index % 5000 == 0:
             tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
-        feature = convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, mode)
-
-        def create_int_feature(values):
-            f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
-            return f
-
-        features = collections.OrderedDict()
-        features["input_ids"] = create_int_feature(feature.input_ids)
-        features["input_mask"] = create_int_feature(feature.input_mask)
-        features["segment_ids"] = create_int_feature(feature.segment_ids)
-        features["label_ids"] = create_int_feature(feature.label_ids)
-        # features["label_mask"] = create_int_feature(feature.label_mask)
-        tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+        feature = convert_single_example_to_feature(ex_index, example, label_map, max_seq_length, tokenizer, mode)
+        tf_example = convert_feature_to_tf_example(feature)
         writer.write(tf_example.SerializeToString())
 
 
@@ -383,7 +361,6 @@ def file_based_input_fn_builder(input_file, seq_length, is_training, drop_remain
         "input_mask": tf.FixedLenFeature([seq_length], tf.int64),
         "segment_ids": tf.FixedLenFeature([seq_length], tf.int64),
         "label_ids": tf.FixedLenFeature([seq_length], tf.int64),
-        # "label_mask": tf.FixedLenFeature([seq_length], tf.int64),
     }
 
     def _decode_record(record, name_to_features):
@@ -531,6 +508,7 @@ def create_model(bert_config, is_training, input_ids, input_mask,
     print('num labels:%d' % num_labels)
     print('#' * 20)
     return (loss, per_example_loss, logits, trans, pred_ids)
+
 
 def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_tpu,
@@ -775,14 +753,6 @@ def main(_):
             json.dump(data_config, fd)
 
     if FLAGS.do_predict:
-        token_path = os.path.join(FLAGS.output_dir, "token_test.txt")
-        if os.path.exists(token_path):
-            os.remove(token_path)
-
-        with codecs.open('./output/label2id.pkl', 'rb') as rf:
-            label2id = pickle.load(rf)
-            id2label = {value: key for key, value in label2id.items()}
-
         # prepare predict_input_fn
         predict_examples = processor.get_test_examples(FLAGS.data_dir)
         predict_file = os.path.join(FLAGS.output_dir, "predict.tf_record")
@@ -822,6 +792,9 @@ def main(_):
         print('*' * 20)
         print('type of result:%s, type of predict_examples:%s' % (type(result), type(predict_examples)))
         print('*' * 20)
+        with codecs.open('./output/label2id.pkl', 'rb') as rf:
+            label2id = pickle.load(rf)
+            id2label = {value: key for key, value in label2id.items()}
         with codecs.open(output_predict_file, 'w', encoding='utf-8') as writer:
             for predict_example, prediction in zip(predict_examples, result):
                 writer.write(predict_example.text + '\n')
